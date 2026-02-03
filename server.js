b1857dfc3a0ba61ec4274164b4b5c8af9cc8f51f
Sauher <schaueroliver@turr.hu>
2026-02-03 09:13:13 +0100

    Jo

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+var cors = require('cors')
+
+const app = express()
+
+const tables = require('./modules/tables')
+const logger = require('./utils/logger')
+
+app.use(cors());
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.use('/uploads', express.static('uploads'));
+app.use('/',tables)
+
+
+
+app.listen(process.env.PORT, () => {
+    logger.info(`Server is listening on http://localhost:${process.env.PORT}`)
+});
